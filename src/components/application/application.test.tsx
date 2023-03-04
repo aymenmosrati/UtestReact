4ee0d44bb8052eba5c6a2b8ae8237755cb3dee1f
Taki Academy <takiacademy@Takis-Mac-mini.local>
2023-03-04 18:09:11 +0100

    first commit

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,22 +1,71 @@
 import { render, screen } from "@testing-library/react";
 import { Application } from "./application";
+
+// 8 different query methods : Order of priority for using queries
+// RTL getBy and getAllBy Queries(RTL: react testing library)
+//1 getByRole                  / getAllByRole
+//2 getByLabelText             / getAllByLabelText
+//3 getByPlaceholderText       / getAllByPlaceholderText
+//4 getByText                  / getAllByText
+//5 getByDisplayValue          / getAllByDisplayValue
+//6 getByAltText               / getAllByAltText
+//7 getByTitle                 / getAllByTitle
+//8 getByTestId                / getAllByTestId
+
 describe("Application", () => {
+  // getByRole Options: name, level, hidden, selected, checked, pressed
   test("renders correctly", () => {
     render(<Application />);
+    // const pageHeading = screen.getByRole("heading", {
+    //   name: "Job application form",
+    // });
+    // expect(pageHeading).toBeInTheDocument();
+
+    // const sectionHeading = screen.getByRole("heading", {
+    //   name: "Section",
+    // });
+    // expect(sectionHeading).toBeInTheDocument();
+
+    // OR
+
     const pageHeading = screen.getByRole("heading", {
-      name: "Job application form",
+      level: 1,
     });
     expect(pageHeading).toBeInTheDocument();
 
     const sectionHeading = screen.getByRole("heading", {
-      name: "Section",
+      level: 2,
     });
     expect(sectionHeading).toBeInTheDocument();
+
+    //getByText works just in paragraph p or div or span element
+    const paragraphElement = screen.getByText("All fields are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
+    const imageElement = screen.getByAltText("a person with a laptop");
+    expect(imageElement).toBeInTheDocument();
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
 
     const nameElement = screen.getByRole("textbox", {
       name: "Name",
     });
     expect(nameElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    const nameElement4 = screen.getByDisplayValue("Vishwas");
+    expect(nameElement4).toBeInTheDocument();
 
     const bioElement = screen.getByRole("textbox", {
       name: "Bio",
@@ -28,6 +77,11 @@ describe("Application", () => {
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -5,17 +5,27 @@ export const Application = () => {
     <>
       <h1>Job application form</h1>
       <h2>Section</h2>
+      <p>All fields are mandatory</p>
+      <span title="close">X</span>
+      <img src="https://via.placeholder.com/150" alt="a person with a laptop" />
+      <div data-testid="custom-element">Custom HTML element</div>
       <form>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            placeholder="Fullname"
+            value="Vishwas"
+            onChange={() => {}}
+          />
         </div>
         <div>
           <label htmlFor="bio">Bio</label>
           <textarea id="bio" name="bio" />
         </div>
         <div>
-          <label htmlFor="job-location">Job location</label>
+          <label htmlFor="job-location">Name</label>
           <select id="job-location">
             <option value="">Select a country</option>
             <option value="US">United States</option>
@@ -27,8 +37,8 @@ export const Application = () => {
         </div>
         <div>
           <label>
-            <input type="checkbox" id="terms" />
-            Iagree to the terms and conditions
+            <input type="checkbox" id="terms" />I agree to the terms and
+            conditions
           </label>
         </div>
         <button>Submit</button>
